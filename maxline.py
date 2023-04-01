aa8770c2ed4024ba7ab3c53cdf6f50985920f6e2
nazhot <15216154+nazhot@users.noreply.github.com>
2023-04-01 06:15:02 -0600

    Finished Maxline Scraper
    Closes #24
    Finished up the scraper for Maxline Brewing, appears to be returning
    everything in the form it should be. Next update will be to plug into
    the importer and running it.

--- maxline.py
+++ maxline.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import datetime
 import common
+import re
 
 name = "Maxline Brewing"
 
 def getEventData(_):
-    defaultThursTime = "18:30"
-    defaultSunTime   = "15:00"
+    defaultThuTime    = "18:30"
+    defaultSunTime    = "15:00"
+    customTimePattern = re.compile(".*\(.*-.*\).*") #pattern to see if there is a custom time in the event title
 
     events       = []
     defaultEvent = common.getDefaultEvent(name, "2724 McClelland Dr #190, Fort Collins, CO 80525")
@@ -19,7 +21,7 @@ def getEventData(_):
     eventBodyElement = soup.find("tbody")
 
     for eventElement in eventBodyElement.find_all("tr"):
-        eventInfoList        = eventElement.find_all("td")
+        eventInfoList        = eventElement.find_all("td") #everything is in td's, need to just use indexes
         eventDateElement     = eventInfoList[0]
         eventBandNameElement = eventInfoList[1]
         eventBandLinkElement = eventInfoList[2]
@@ -28,15 +30,32 @@ def getEventData(_):
         if eventBandName == "":
             continue
         
-        eventDateString     = eventDateElement.get_text()
+        eventDateString     = eventDateElement.get_text() #format: {day of week (short)} {month}/{day}/{year}
         eventBandLink       = eventBandLinkElement.get_text()
         if eventDateString.count(" ") != 1:
             print(f'Date in Maxline does not contain 1 space: {eventDateString}. Event: {eventBandName}')
             continue
 
-        eventDay, eventDate = eventDateString.split(" ")
-        startDateTime = ""
-        endDateTime = ""
+        eventDayOfWeek, eventDate       = eventDateString.split(" ")
+        eventMonth, eventDay, eventYear = eventDate.split("/")
+        eventMonth                      = eventMonth.rjust(2, "0")
+        eventDay                        = eventDay.rjust(2, "0")
+        eventYear                       = "20" + eventYear
+
+        if customTimePattern.match(eventBandName): #format: ({start}-{end}pm)
+            timeString         = eventBandName.split("(")[-1].split(")")[0].lower().replace("pm", "")
+            startTime, endTime = timeString.split("-")
+            startTime          = common.convertPMTimeWithoutMeridian(startTime)
+            endTime            = common.convertPMTimeWithoutMeridian(endTime)
+        elif eventDayOfWeek == "Thu":
+            startTime = defaultThuTime
+            endTime   = common.addHoursToTime(startTime, common.defaultEventHours)
+        elif eventDayOfWeek == "Sun":
+            startTime = defaultSunTime
+            endTime   = common.addHoursToTime(startTime, common.defaultEventHours)
+        
+        startDateTime = common.convertToEventDateTime(eventDay, eventMonth, eventYear, startTime)
+        endDateTime   = common.convertToEventDateTime(eventDay, eventMonth, eventYear, endTime)
 
         eventCopy                = defaultEvent.copy()
         eventCopy["summary"]     = eventBandName
@@ -47,5 +66,3 @@ def getEventData(_):
         events.append(eventCopy)
 
     return events
-
-getEventData("")
