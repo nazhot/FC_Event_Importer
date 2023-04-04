1e347572b4ae786ad75f2b862126ce664ad6bab6
nazhot <15216154+nazhot@users.noreply.github.com>
2023-04-04 14:42:06 -0600

    Changed to Dedicated FC Calendar
    Created a new email, got new credentials, and turned back on recurring
    events.

--- importer.py
+++ importer.py
@@ -169,7 +169,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'credentials1.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
@@ -185,7 +185,7 @@ def main():
 
         venues = [magicRat, comedyFort, avogadros, aggieTheatre, maxline]
 
-        #uploadRecurringEvents(service, calendarDict)
+        uploadRecurringEvents(service, calendarDict)
         #return
 
         with webdriver.Firefox(options=options) as browser:
