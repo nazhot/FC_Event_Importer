6c024a42e9a56d55bcb5652d3e44fd5b6a7794e0
nazhot <15216154+nazhot@users.noreply.github.com>
2023-04-03 19:27:10 -0600

    Upgraded Magic Rat from 3 to 6 Events
    Closes #9
    Was able to upgrade the number of events gotten from Magic Rat from 3 to
    6, just by changing the url. Unfortunately, trying to load more failed.
    I tried using selenium to click the load more button, and it ends up
    causing a connection error. I can't figure out what is causing it, will
    create a new issue to try and increase the range further.

--- magicRat.py
+++ magicRat.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import common
 import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 name = "Magic Rat"
 
@@ -13,9 +17,24 @@ def getEventData(browser):
     url = "https://www.theelizabethcolorado.com/magicratlivemusic/calendar-page"
 
     browser.get(url)
+    # cookieAcceptButton = browser.find_element(By.ID, "truste-consent-button")
+    # cookieAcceptButton.click()
+    # for i in range(5):
+    #     try:
+    #         loadMoreButton = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "calendar-load-more")))
+    #         print(loadMoreButton.text)
+    #         #browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #         loadMoreButton.click()
+    #         #browser.execute_script("arguments[0].click();", loadMoreButton)
+    #     finally:
+    #         browser.quit()
     html = browser.page_source
 
     soup = BeautifulSoup(html, "html.parser")
+
+
+  
+
 
     for eventContainer in soup.find_all("div", class_="event-info-container"):
         bandContainer = eventContainer.find("h3")
